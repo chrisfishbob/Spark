bcb31ca0f1f2d0512cedabbd0750faec7700e0b6
Chris Hsu <chrisfishbob@gmail.com>
2022-12-18 17:16:28 -0800

    Added IfC matching clause in parse

--- spark.py
+++ spark.py
@@ -1,15 +1,33 @@
-from sexpdata import loads, dumps
+from typing import NamedTuple
+from sexpdata import loads, dumps, Symbol
 
+class IfC(NamedTuple):
+    test: any
+    then: any
+    otherwise: any
+
+class IdC(NamedTuple):
+    name: Symbol 
+
+
+    
 def main():
     s = loads('1')
-    print(parse(s))
+    s1 = loads('(if true 1 2)')
+    # print(s1)
+    # print(type(s1[0]))
+    print(parse(s1))
 
 def parse(sexp):
     match sexp:
         case int() | float() | str():
             return sexp
+        case [Symbol(), test_cond, then, otherwise] if dumps(sexp[0]) == "if":
+            return IfC(parse(test_cond), parse(then), parse(otherwise)) 
+        case Symbol():
+            return IdC(sexp)
         case _:
-            print("Error while parsing")
+            return "Error while parsing"
 
 
 if __name__ == "__main__":
--- unittests.py
+++ unittests.py
@@ -1,5 +1,6 @@
 import unittest
 import spark
+from sexpdata import Symbol
 
 class SparkTests(unittest.TestCase):
     def test_parse_int(self):
@@ -7,6 +8,9 @@ class SparkTests(unittest.TestCase):
         self.assertEqual(23, spark.parse(23))
         self.assertEqual(100, spark.parse(100))
         self.assertEqual(4000000, spark.parse(4000000))
+
+    def test_parse_symbol(self):
+        self.assertEqual(Symbol('true'), spark.parse(1))
 
 
 if __name__ == "__main__":
